8f491daca1c9916241602da7527621bf26b1b1d5
Tim Freund <tim@freunds.net>
2014-01-12 12:23:23 -0500

    FilteAdd filtering to photofuse-ls

--- README.rst
+++ README.rst
@@ -8,8 +8,10 @@ Thoughts on EXIF reader libraries
 Dev Notes
 =========
 
+import mimetypes
 import pyexiv2
-img = pyexiv2.ImageMetaData('~/tmp/20090426-193954-00.jpg')
+mtype = mimetypes.guess_type('~/tmp/20090426-193954-00.jpg')[0]
+img = pyexiv2.ImageMetadata('~/tmp/20090426-193954-00.jpg')
 img.read()
 img['Exif.Image.Rating'].value
 img['Iptc.Application2.Keywords'].values
--- photofuse/cli.py
+++ photofuse/cli.py
@@ -1,3 +1,6 @@
+import mimetypes
+import os
+import pyexiv2
 import sys
 
 from optparse import OptionParser, Option
@@ -8,15 +11,26 @@ def validate_destination(options, args):
         return False
     return True
 
+def validate_rating(options, args):
+    if options.rating:
+        options.rating = int(options.rating)
+    return True
+
 def validate_source(options, args):
     if not options.source:
         print "A source is required"
         return False
     return True
 
+def validate_tags(options, args):
+    if args:
+        options.tags = set(args)
+    return True
 
 def parse_options(options=[], validators=[], usage=None):
     validators.insert(0, validate_source)
+    validators.insert(0, validate_rating)
+    validators.insert(0, validate_tags)
 
     parser = OptionParser()
     parser.add_option('-s', '--source-dir', dest='source', help='Photo directory')
@@ -44,6 +58,31 @@ def ls(argv=sys.argv):
 
     if args:
         print "\twith the tag(s): %s" % " ".join(args)
+
+    for dirpath, dirnames, filenames in os.walk(opts.source):
+        for name in filenames:
+            full_path = os.path.join(dirpath, name)
+            if mimetypes.guess_type(full_path)[0] == 'image/jpeg':
+                visible = True
+                rating = None
+                tags = []
+
+                img = pyexiv2.ImageMetadata(full_path)
+                img.read()
+
+                if img.keys().count('Exif.Image.Rating'):
+                    rating = img['Exif.Image.Rating'].value
+                if img.keys().count('Iptc.Application2.Keywords'):
+                    tags = img['Iptc.Application2.Keywords'].values
+                
+                if opts.rating and (opts.rating > rating or not rating):
+                    visible = False
+
+                if opts.tags and opts.tags.isdisjoint(set(tags)):
+                    visible = False
+                    
+                if visible:
+                    print "%s Rating(%s) Tags(%s)" % (full_path, rating, ", ".join(tags))
 
 def photofuse(argv=sys.argv):
     options, args = parse_options([Option()], validators=[destination_validator])
